5007b7a74491f5d8a9d1768b686ad2c970fe4638
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2013-04-25 15:26:25 +0100

    Pretty Print

--- response.py
+++ response.py
@@ -77,7 +77,8 @@ class SimpleResponse:
         if '%s%s' % (sys.version_info[0], sys.version_info[1]) == '25':
             fp.write(json.write(self.commands))
         else:
-            fp.write(json.dumps(self.commands))
+            fp.write(json.dumps(self.commands, sort_keys=True,
+...                  indent=4, separators=(',', ': ')))
         fp.close()
 
     def help(self):
