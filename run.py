eb13d74ddaf20740b1a3c7be1a71b61e80443ec9
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2013-07-21 03:12:05 +0000

    Convert Dindrabot to use twisted Framework

--- run.py
+++ run.py
@@ -1,20 +1,21 @@
-from dindrabot import DindraBot
+from dindrabotfactory import DindraBotFactory
+from twisted.internet import reactor
 import sys
 
-
-def main(channel, name, server, port):
-    bot = DindraBot(channel, name, server, int(port))
-
-    bot.start()
-
 if __name__ == "__main__":
+
     channel = "#dindraheart"
-    name = "DindraBot"
+    nickname = "DindraBot"
+
     server = "dindraheart.jtvirc.com"
-    port = "6667"
+    port = 6667
     if len(sys.argv) > 1: channel = sys.argv[1]
-    if len(sys.argv) > 2: name = sys.argv[2]
+    if len(sys.argv) > 2: nickname = sys.argv[2]
     if len(sys.argv) > 3: server = sys.argv[3]
     if len(sys.argv) > 4: port = sys.argv[4]
-    main(channel, name, server, port)
 
+    f = DindraBotFactory(channel, nickname)
+
+    reactor.connectTCP(server, port, f)
+
+    reactor.run()
