7663724e4d0e05d84ebe8b2961881d52f532ac8a
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2013-07-21 03:12:05 +0000

    Convert Dindrabot to use twisted Framework

--- dindrabot.py
+++ dindrabot.py
@@ -1,49 +1,33 @@
-import os
-from ircbot import SingleServerIRCBot
-from response import SimpleResponse
-from irclib import nm_to_n, nm_to_h, irc_lower, ip_numstr_to_quad, ip_quad_to_numstr
+from twisted.words.protocols import irc
 
-class DindraBot(SingleServerIRCBot):
+class DindraBot(irc.IRCClient):
 
-    def __init__(self, channel, nickname, server, port=6667):
+    response = None
+    mods = []
 
-        pth = os.path.join(os.path.split(__file__)[0], "password.txt")
-        if not os.path.exists(pth):
-            self.password = None
-        else:
-            fp = open(pth)
-            self.password = fp.read().strip()
-            fp.close()
-        print "Connecting to", channel
-        self.channel = channel
-        self.nickname = nickname
+    def signedOn(self):
+        self.join(self.factory.channel)
 
-        self.response = SimpleResponse()
+    def privmsg(self, speaker, channel, msg):
 
-        SingleServerIRCBot.__init__(self, [(server, port, self.password)], nickname, nickname)
+        if channel != self.factory.channel: return
 
-    def on_nicknameinuse(self, c, e):
-        c.nick(c.get_nickname() + "_")
-
-    def on_welcome(self, c, e):
-        print "Connected"
-        c.join(self.channel)
-        print
-
-    def on_join(self, c, e):
-        ch = e.target()
-
-    def on_pubmsg(self, c, e):
-        speaker = nm_to_n(e.source())
-        channel = e.target()
-        spoken = e.arguments()[0]
-        if channel != self.channel: return
-        melower = c.get_nickname().lower()
-        if not spoken.startswith("!"):
+        if not msg.startswith("!"):
             return
 
-        spoken = spoken[1:].strip()
+        msg = msg[1:]
 
-        if self.response.has_response(spoken):
-            c.privmsg(channel, self.response.get_response(spoken, speaker, self.channels[channel].is_oper(speaker)))
+        is_mod = speaker.split('!')[0] in self.mods
+
+        if self.response.has_response(msg):
+            self.msg(channel, self.response.get_response(msg, speaker, is_mod))
+
+
+    def modeChanged(self, user, channel, set, modes, args):
+        if channel != self.factory.channel:
+            return
+
+        if set:
+            if args not in self.mods:
+                self.mods.append(args)
 
